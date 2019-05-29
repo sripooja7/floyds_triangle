e10041ff958df742c09301287642a792b81b2793
sripooja7 <33750194+sripooja7@users.noreply.github.com>
2019-05-29 18:49:31 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j,n,k=1;
+    printf("Enter the number of rows\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",k);
+            k++;
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
